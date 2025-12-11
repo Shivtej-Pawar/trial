4814966e0e286f38228f647eb92436da082a9351
Shivtej-Pawar <shivtejapawar5@gmail.com>
2025-12-11 19:03:26 +0530

    geometry file

---
+++ geomain.py
@@ -0,0 +1,7 @@
+import  geometry as g
+
+l=float(input('enter the length:'))
+b=float(input('enter the breadth:'))
+
+g.cal_rect_area(l,b)
+g.cal_rect_peri(l,b)
---
+++ geometry.py
@@ -0,0 +1,8 @@
+def cal_rect_area(l,b):
+       print('The area of rectangle is:',l*b)
+
+def cal_rect_peri(l,b):
+       print('The area of rectangle is:',2*(l+b))
+    
+         
+
